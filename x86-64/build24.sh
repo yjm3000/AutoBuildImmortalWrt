95581f3996311e58507bc0b21fcfdeaaeaec11c3
James Yu <149986319+yjm3000@users.noreply.github.com>
2025-09-09 04:37:44 +0800

    Update build24.sh

--- x86-64/build24.sh
+++ x86-64/build24.sh
@@ -44,7 +44,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 
 # ============= imm仓库内的插件==============
 # 定义所需安装的包列表 下列插件你都可以自行删减
-# 2025年9月9日自行修改增加库内插件
+# ======== 2025年9月9日自行修改增加库内插件 ========
 PACKAGES=""
 PACKAGES="$PACKAGES curl"
 PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
